93dbb66599f4d20d4ccdcdf8b75c46064f98e335
VetaliiP <pvty36@gmail.com>
2025-04-03 22:33:32 +0200

    Remove errors

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,18 +1,9 @@
 import { FC, } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import { Layout } from "./components/Layout/Layout";
 import { Main } from './pages/Main/Main';
-import { Search } from './pages/Search/Search';
-import { Messages } from './pages/Messages/Messages';
-import { Notification } from './pages/Notification/Notification';
-import { Publication } from './pages/Publication/Publication';
-import { Profile } from './pages/Profile/Profile';
-import { More } from './pages/More/More';
-import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import './App.css'
 import { Auth } from './pages/Auth/Auth';
-import { useAuth } from './context/AuthContext';
 import { Registration } from './pages/Auth/Registration';
 
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import ReactDOM from "react-dom/client";
 import { AppRouter } from "./AppRouter"
 import "./index.css";
